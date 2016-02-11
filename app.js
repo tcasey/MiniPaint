b3097bd769227e850ef727d6817ecfa555229afd
tcasey <trevort.casey@gmail.com>
2016-02-11 13:51:53 -0700

    feat: drag

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-	var color = "white";
-		//selector //listener //callback
-	$('.box').on("click", function(){
-		$(this).addClass(color); // function body
-	})
+  var color = 'white';
+  var colors = 'white green red blue yellow';
+  var box = $('.box');
 
-  $('.box').on('click', function() {
-   $(this).addClass(color);
- })
+  var drag = false;
+  box.mouseenter(function() {
+    drag = true;
+  })
+  box.mousedown(function() {
+    drag = false;
+  })
 
- var colors = 'white green red blue yellow'
 
-  $('.box').on('dblclick', function() {
-    $(this).removeClass(colors);
+
+
+  $('#red').on('click', function() {
+    color = 'red';
+  });
+  $('#blue').on('click', function() {
+    color = 'blue';
+  });
+  $('#green').on('click', function() {
+    color = 'green';
+  });
+  $('#yellow').on('click', function() {
+    color = 'yellow';
+  });
+  $('#white').on('click', function() {
+    color = 'white';
+  });
+  box.on('click', function() {
+    $(this).addClass(color);
+  });
+
+  box.on('dblclick', function() {
+    $(this).removeClass(color);
+  });
+  box.on('mouseenter', function() {
+    if (drag) {
+      $(this).addClass(color);
+    }
   })
 
   $('#reset').on('click', function() {
-    $('.box').removeClass(colors)
-  })
-  
- $('#red').on('click', function() {
-   color = 'red';
- })
-
- $('#blue').on('click', function() {
-   color = 'blue';
- })
-
- $('#green').on('click', function() {
-   color = 'green';
- })
-
- $('#yellow').on('click', function() {
-   color = 'yellow';
- })
-
- $('#white').on('click', function() {
-   color = 'white';
- })
-
-
-
-
+    box.removeClass(colors);
+  });
 })

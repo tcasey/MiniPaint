85fabb1972b44a3d54e838d89c12ad4044db2fcb
tcasey <trevort.casey@gmail.com>
2016-02-11 12:09:18 -0700

    Did it again

--- app.js
+++ app.js
@@ -1,36 +1,46 @@
 $(document).ready(function(){
 
-
+	var color = "white";
 		//selector //listener //callback
+	$('.box').on("click", function(){
+		$(this).addClass(color); // function body
+	})
 
-var color = 'white';
-var colors = 'white green red blue yellow';
-
-$(document).ready(function() {
-  $('#red').on('click', function() {
-    color = 'red';
-  });
-  $('#blue').on('click', function() {
-    color = 'blue';
-  });
-  $('#green').on('click', function() {
-    color = 'green';
-  });
-  $('#yellow').on('click', function() {
-    color = 'yellow';
-  });
-  $('#white').on('click', function() {
-    color = 'white';
-  });
   $('.box').on('click', function() {
-    $(this).addClass(color);
-  });
+   $(this).addClass(color);
+ })
+
+ var colors = 'white green red blue yellow'
 
   $('.box').on('dblclick', function() {
-    $(this).removeClass(color);
-  });
+    $(this).removeClass(colors);
+  })
 
   $('#reset').on('click', function() {
-    $('.box').removeClass(colors);
-  });
+    $('.box').removeClass(colors)
+  })
+  
+ $('#red').on('click', function() {
+   color = 'red';
+ })
+
+ $('#blue').on('click', function() {
+   color = 'blue';
+ })
+
+ $('#green').on('click', function() {
+   color = 'green';
+ })
+
+ $('#yellow').on('click', function() {
+   color = 'yellow';
+ })
+
+ $('#white').on('click', function() {
+   color = 'white';
+ })
+
+
+
+
 })
